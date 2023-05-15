5812d58f2030e7cc6d7e1f9eb9d5712f62a6d764
lesnikovskiy <lesnikovski@gmail.com>
2023-05-15 23:18:35 +0300

    Add animated background layers

--- dapper-dasher/dasher.cpp
+++ dapper-dasher/dasher.cpp
@@ -83,6 +83,15 @@ int main()
 
     int velocity{};
 
+    Texture2D background = LoadTexture("textures/far-buildings.png");
+    float bgX{};
+
+    Texture2D midground = LoadTexture("textures/back-buildings.png");
+    float mgX{};
+
+    Texture2D foreground = LoadTexture("textures/foreground.png");
+    float fgX{};
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
@@ -93,6 +102,43 @@ int main()
         BeginDrawing();
 
         ClearBackground(WHITE);
+
+        // calc background pos
+        bgX -= 20 * dT;
+        if (bgX <= -background.width * 2)
+        {
+            bgX = 0.0;
+        }
+
+        mgX -= 20 * dT;
+        if (mgX <= -midground.width * 2)
+        {
+            mgX = 0.0;
+        }
+
+        fgX -= 20 * dT;
+        if (fgX <= -foreground.width * 2)
+        {
+            fgX = 0.0;
+        }
+
+        // draw the background
+        Vector2 bg1Pos{bgX, 0.0};
+        DrawTextureEx(background, bg1Pos, 0.0, 2.0, WHITE);
+        Vector2 bg2Pos{bgX + background.width * 2, 0.0};
+        DrawTextureEx(background, bg2Pos, 0.0, 2.0, WHITE);
+
+        // draw the middleground
+        Vector2 mg1Pos{mgX, 0.0};
+        DrawTextureEx(midground, mg1Pos, 0.0, 2.0, WHITE);
+        Vector2 mg2Pos{mgX + midground.width * 2, 0.0};
+        DrawTextureEx(midground, mg2Pos, 0.0, 2.0, WHITE);
+
+        // Draw the foreground
+        Vector2 fg1Pos{fgX, 0.0};
+        DrawTextureEx(foreground, fg1Pos, 0.0, 2.0, WHITE);
+        Vector2 fg2Pos{fgX + foreground.width * 2, 0.0};
+        DrawTextureEx(foreground, fg2Pos, 0.0, 2.0, WHITE);
 
         // perform ground check
         if (isOnGround(scarfyData, windowDimensions[1]))
@@ -148,6 +194,9 @@ int main()
         EndDrawing();
     }
 
+    UnloadTexture(background);
+    UnloadTexture(midground);
+    UnloadTexture(foreground);
     UnloadTexture(nebularTexture);
     UnloadTexture(scarfyTexture);
     CloseWindow();
