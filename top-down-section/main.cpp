b864001f423788d6460ba4d7d75524779bc5fbd3
lesnikovskiy <lesnikovski@gmail.com>
2023-05-16 23:25:16 +0300

    move the map

--- top-down-section/main.cpp
+++ top-down-section/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -9,6 +10,10 @@ int main()
 
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
 
+    Vector2 mapPos{0.0, 0.0};
+
+    float speed{4.0};
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
@@ -17,7 +22,22 @@ int main()
 
         ClearBackground(WHITE);
 
-        Vector2 mapPos{0.0, 0.0};
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A))
+            direction.x -= 1.0;
+        if (IsKeyDown(KEY_D))
+            direction.x += 1.0;
+        if (IsKeyDown(KEY_W))
+            direction.y -= 1.0;
+        if (IsKeyDown(KEY_S))
+            direction.y += 1.0;
+
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         EndDrawing();
