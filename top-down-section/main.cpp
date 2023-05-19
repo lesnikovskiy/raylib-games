b7926599b661188220c29236344935a0691b1759
lesnikovskiy <lesnikovski@gmail.com>
2023-05-19 23:26:14 +0300

    animate character on map

--- top-down-section/main.cpp
+++ top-down-section/main.cpp
@@ -9,10 +9,23 @@ int main()
     InitWindow(windowWidth, windowHeight, "Open World Game");
 
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
-
     Vector2 mapPos{0.0, 0.0};
-
     float speed{4.0};
+
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
+    Texture2D knight = knight_idle;
+    Vector2 knightPos{
+        (float)windowWidth / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f),
+        (float)windowHeight / 2.0f - 4.0f * (0.5f * (float)knight.height)};
+
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    const float updateTime{1.f/12.f};
 
     SetTargetFPS(60);
 
@@ -36,14 +49,40 @@ int main()
         {
             // set mapPos = mapPos - direction
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            knight = knight_run;
+        }
+        else
+        {
+            knight = knight_idle;
         }
 
+        // draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            runningTime = 0.f;
+            frame++;
+            if (frame > maxFrames)
+            {
+                frame = 0;
+            }
+        }
+
+        // draw the character
+        Rectangle source{(float)knight.width/6.f * frame, 0.f, rightLeft * (float)knight.width / 6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width / 6.0f, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
         EndDrawing();
     }
 
     UnloadTexture(map);
+    UnloadTexture(knight_idle);
+    UnloadTexture(knight_run);
 
     CloseWindow();
 
