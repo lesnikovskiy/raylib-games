dba87c4fdf95f6cab9f6dc1d39394c13f0d11b72
lesnikovskiy <lesnikovski@gmail.com>
2023-05-22 21:24:57 +0300

    add classes

--- top-down-section/main.cpp
+++ top-down-section/main.cpp
@@ -1,6 +1,26 @@
 #include "raylib.h"
 #include "raymath.h"
 
+class Character
+{
+public:
+    Vector2 getWorldPos() { return worldPos; }
+
+private:
+    Texture2D texture;
+    Texture2D idle;
+    Texture2D run;
+    Vector2 screenPos;
+    Vector2 worldPos;
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f / 12.f};
+};
+
 int main()
 {
     const int windowWidth{384};
@@ -25,7 +45,7 @@ int main()
     float runningTime{};
     int frame{};
     int maxFrames{6};
-    const float updateTime{1.f/12.f};
+    const float updateTime{1.f / 12.f};
 
     SetTargetFPS(60);
 
@@ -73,7 +93,7 @@ int main()
         }
 
         // draw the character
-        Rectangle source{(float)knight.width/6.f * frame, 0.f, rightLeft * (float)knight.width / 6.f, (float)knight.height};
+        Rectangle source{(float)knight.width / 6.f * frame, 0.f, rightLeft * (float)knight.width / 6.f, (float)knight.height};
         Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width / 6.0f, 4.0f * (float)knight.height};
         DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
